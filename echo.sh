8bde4b25e760fdd0bf32755d31302a9dc492d1c7
chetantarale <chetan@shippable.com>
2017-02-14 10:58:42 +0530

    Adds pusg tag

--- echo.sh
+++ echo.sh
@@ -1,2 +1,14 @@
-echo 'Yo mon Whatsap!! on 25/10/2016!!'
-sleep 2m
+export RES_REPO=sample-script
+export RES_REPO=v1.0
+
+
+tag_push(){
+  pushd ./IN/$RES_BASE_REPO/gitRepo
+  echo "pushing git tag $VERSION to $RES_REPO"
+  #git checkout $(git rev-list -n 1 $REL_VER)
+  git tag $VERSION
+  git push origin $VERSION
+  echo "completed pushing git tag $VERSION to $RES_BASE_REPO at $REL_VER"
+  popd
+}
+tag_push
