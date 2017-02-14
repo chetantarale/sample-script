68813c49fe12f1c9965f3ae63b16674f16b9ae0c
chetantarale <chetan@shippable.com>
2017-02-14 11:21:18 +0530

    add keys

--- echo.sh
+++ echo.sh
@@ -1,6 +1,15 @@
 export RES_REPO=sample-script
 export VERSION=v1.0
 
+set_up() {
+  sudo useradd -m -s /usr/bin/git-shell git
+  sudo -u git bash
+  cd ~
+  ## cd /home/git
+  mkdir -p .ssh
+  mv ./IN/$RES_REPO/gitRepo/authorized_keys .ssh/authorized_keys
+  ## Paste your public key and save
+}
 
 tag_push(){
   pushd ./IN/$RES_REPO/gitRepo
@@ -8,7 +17,8 @@ tag_push(){
   #git checkout $(git rev-list -n 1 $REL_VER)
   git tag $VERSION
   git push origin $VERSION
-  echo "completed pushing git tag $VERSION to $RES_BASE_REPO at $REL_VER"
+  echo "completed pushing git tag $VERSION to $RES_REPO"
   popd
 }
+set_up
 tag_push
