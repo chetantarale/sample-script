196bceb995a55f8b92100f4f254f912fc5bd31f8
chetantarale <chetan@shippable.com>
2017-02-14 11:07:55 +0530

    fix repo name

--- echo.sh
+++ echo.sh
@@ -3,7 +3,7 @@ export RES_REPO=v1.0
 
 
 tag_push(){
-  pushd ./IN/$RES_BASE_REPO/gitRepo
+  pushd ./IN/$RES_REPO/gitRepo
   echo "pushing git tag $VERSION to $RES_REPO"
   #git checkout $(git rev-list -n 1 $REL_VER)
   git tag $VERSION
