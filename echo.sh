344b0a56fafc90f0c0e8b93757e503dec8c46a15
chetantarale <chetan@shippable.com>
2017-02-14 14:45:59 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -3,9 +3,9 @@ export VERSION=v3.0
 export KEY_INTEGRATION=rsakey
 
 configure_node_creds() {
-  ls IN/rsakey
-  cat IN/rsakey/integration.env
-  cat IN/rsakey/integration.json
+#   ls IN/rsakey
+#   cat IN/rsakey/integration.env
+#   cat IN/rsakey/integration.json
   echo "Extracting Key"
   echo "-----------------------------------"
   local creds_path="IN/$KEY_INTEGRATION/integration.env"
@@ -37,9 +37,8 @@ tag_push(){
   git remote add origin git@github.com:chetantarale/testRepo.git
   #git remote add origin https://chetantarale:xxxxx@github.com/chetantarale/testRepo.git
   git tag $VERSION
-  #git push origin $VERSION
-  echo $KEY_FILE_PATH
-  ssh-agent $(ssh-add $KEY_FILE_PATH; git push origin git@github.com:chetantarale/testRepo.git)
+  git push origin $VERSION
+  #ssh-agent $(ssh-add $KEY_FILE_PATH; git push origin git@github.com:chetantarale/testRepo.git)
   echo "completed pushing git tag $VERSION to $RES_REPO"
   popd
 }
