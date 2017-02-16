cc5278b0b31a7b2c26906b975dee21b71c9d5c1c
chetantarale <chetan@shippable.com>
2017-02-16 17:19:21 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -63,7 +63,7 @@ clean_old_tags() {
       CURR_TAG=$(echo $line | awk '{print $1}')
       CURR_TAG_TIMESTAMP=$(echo $line | awk '{print $2}')
       if [[ $CURR_TAG_TIMESTAMP != "" && $CURR_TAG_TIMESTAMP != null && $CURR_TAG_TIMESTAMP -lt $CURR_REL_TAG_TIME ]] ; then
-         echo "$CURR_TAG was pushed before $CURR_REL_TAG"
+         echo "$CURR_TAG was pushed before $VERSION"
       fi
    done
    popd
